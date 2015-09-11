f0edf25ccae12d955fee20d6f2d894f194b92281
Rudi Yardley <contact@rudiyardley.com>
2015-09-11 23:36:45 +0300

    Serve favicons

--- app/server/index.js
+++ app/server/index.js
@@ -3,12 +3,16 @@
 import config from 'app/config';
 import express from 'express';
 import isomorpher from './middleware/isomorpher';
+import favicon from 'serve-favicon';
+
 
 export default function serve () {
   const app = express();
   const c = config();
 
+  app.use(favicon(__dirname + '../../assets/favicon.ico'));
   app.use(isomorpher);
+
 
   const server = app.listen(c.port, function () {
     let {host, port} = server.address();
