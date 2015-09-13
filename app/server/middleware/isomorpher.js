4d1eade612323de69fb530a8bd7a63442099a373
Rudi Yardley <contact@rudiyardley.com>
2015-09-13 23:52:24 +0300

    Tidy up express middleware

--- app/server/middleware/isomorpher.js
+++ app/server/middleware/isomorpher.js
@@ -16,36 +16,44 @@ export default function isomorpher(req, res) {
 
   match({routes, location}, (error, redirectLocation, renderProps) => {
 
+
     if (redirectLocation) {
       res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-    } else if (error) {
-      res.send(500, error.message);
-    } else if (renderProps == null) {
-      res.send(404, 'Not found');
-    } else {
-
-      const wrappers = renderProps.components;
-      const withFetch = wrappers.filter((w) => w.WrappedComponent && w.WrappedComponent.fetchTransitionData);
-      const promises = withFetch
-                        .map((w) => w.WrappedComponent)
-                        .map((Component) => Component.fetchTransitionData());
-
-      Promise.all(promises).then(()=>{
-        res.send(
-          React.renderToStaticMarkup(
-            React.createFactory(Html)({
-              markup: React.renderToString(
-                <Provider store={store}>
-                  {() => <RoutingContext {...renderProps} />}
-                </Provider>
-              ),
-              state: dehydrate('App', store.getState()),
-              title: DocumentTitle.rewind()
-            })
-          )
-        );
-      });
+      return;
     }
+
+    if (error) {
+      res.send(500, error.message);
+      return;
+    }
+
+    if (renderProps == null) {
+      res.send(404, 'Not found');
+      return;
+    }
+
+    const wrappers = renderProps.components;
+    const withFetch = wrappers.filter((w) => w.WrappedComponent && w.WrappedComponent.fetchTransitionData);
+    const promises = withFetch
+                      .map((w) => w.WrappedComponent)
+                      .map((Component) => Component.fetchTransitionData());
+
+    Promise.all(promises).then(()=>{
+      res.send(
+        React.renderToStaticMarkup(
+          React.createFactory(Html)({
+            markup: React.renderToString(
+              <Provider store={store}>
+                {() => <RoutingContext {...renderProps} />}
+              </Provider>
+            ),
+            state: dehydrate('App', store.getState()),
+            title: DocumentTitle.rewind()
+          })
+        )
+      );
+    });
+
   });
 
 }
