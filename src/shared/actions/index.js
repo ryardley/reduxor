ac87475161eff8530a5b1bab27329f88fcefab4b
Rudi Yardley <contact@rudiyardley.com>
2015-09-15 23:13:24 +0300

    Clean up creating client in isomorphic situations.

--- src/shared/actions/index.js
+++ src/shared/actions/index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 import {createAction} from 'redux-actions';
-import {createClient} from '../nector';
-import server from '../../server/middleware/nector';
+import nector from '../nector';
 
-const origin =(typeof window !== 'undefined') ? window.location.origin : server;
-const client = createClient(origin);
+
 export const fetchFollowingAction = createAction('FETCH_FOLLOWERS', () => {
+  const client = nector.createClient();
   return client('fetchFollowing')();
 });
